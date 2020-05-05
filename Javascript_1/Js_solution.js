db92358a3f7b66844c877e12f99c6d8ee3b549a1
Jayesh Jadhav <Jayesh.Jadhav@ey.com>
2020-05-05 11:05:56 +0530

    Updated

--- Javascript_1/Js_solution.js
+++ Javascript_1/Js_solution.js
@@ -1,21 +1,16 @@
-"use strict";
+"use strict"
 
 function stripProperty(obj, prop) {
   // write your code here
-  // let myobj = Object.create(obj, Object.defineProperties({configurable: true}))
-  if (obj.prop) {
-    obj.prop = undefined;
-  }
-  obj = JSON.parse(JSON.stringify(obj));
-  //   for (let key in Object.keys(obj)) {
-  //        if(key === prop) {
-  //            const { prop, ...newobj } = obj;
-  //            // Reflect.deleteProperty(obj, prop);
-  //            // delete obj.prop
-  //            // return newobj;
-  //            break;
-  //        }
-  //   }
-
-  return obj;
+  let myobj = { ...obj }
+  delete myobj[prop]
+  return myobj
 }
+
+let testobj = {
+  foo: 1,
+  bar: 2,
+  baz: 3
+}
+
+console.log(stripProperty(testobj, "bar"))
